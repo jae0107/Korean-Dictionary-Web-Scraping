2308daa1b1f5b75157e8cfd110be38cb5f863c15
jae0107 <jae.c@resvu.io>
2025-03-12 00:59:22 +1030

    roll back

--- src/app/api/initialisers/sequelize.ts
+++ src/app/api/initialisers/sequelize.ts
@@ -11,7 +11,6 @@ function initSequelize(): Sequelize {
   const sequelize: Sequelize = new Sequelize({
     dialect: process.env.DATABASE_DIALECT,
     dialectModule: pg,
-    address: process.env.DATABASE_URL,
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
