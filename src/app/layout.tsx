410a0d7aed1a0570d527fe6b6e38923b791f746c
jae0107 <jae.c@resvu.io>
2025-03-16 02:45:12 +1030

    add preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     description: "학생들의 어휘력을 향상 시키기 위한 웹 어플리케이션",
     images: [
       {
-        url: '<img width="1507" alt="Screenshot 2025-03-16 at 2 27 32 am" src="https://github.com/user-attachments/assets/aea63b71-0029-49a7-a8d9-62cc51f79604" />',
+        url: 'https://github.com/user-attachments/assets/aea63b71-0029-49a7-a8d9-62cc51f79604',
         width: 1507,
         height: 630,
       },
